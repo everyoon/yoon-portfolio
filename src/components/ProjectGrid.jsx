93040e06751fa9963cbf8f189b681cc4c492619c
everyoon <155042473+everyoon@users.noreply.github.com>
2026-01-21 16:25:39 +0900

    새 프로젝트 추가

--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -1,5 +1,12 @@
-import styled from 'styled-components';
-import ProjectCard from './ProjectCard';
+import styled from "styled-components";
+import ProjectCard from "./ProjectCard";
+
+const parseDotDate = (s) => {
+  // "2025.06.04" -> "2025-06-04"
+  const iso = (s || "").trim().replaceAll(".", "-");
+  const t = Date.parse(iso);
+  return Number.isNaN(t) ? 0 : t;
+};
 
 const Grid = styled.div`
   display: grid;
@@ -14,7 +21,9 @@ const Grid = styled.div`
 `;
 
 function ProjectGrid({ projects }) {
-  const sortedProjects = [...projects].sort((a, b) => new Date(b.date) - new Date(a.date));
+  const sortedProjects = [...projects].sort(
+    (a, b) => parseDotDate(b.date) - parseDotDate(a.date)
+  );
 
   return (
     <Grid>
